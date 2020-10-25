ca4dd05fd7c3fbd3809d9cf14db7bd5181599ef5
jnka070 <juansalamea94@gmail.com>
2020-10-25 05:40:01 -0500

    make it better

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ app.set('port', process.env.PORT||3000);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views')); 
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 //middlewares
 app.use(morgan('dev'));
