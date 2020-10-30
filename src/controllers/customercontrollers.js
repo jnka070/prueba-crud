50e24f9f5b7275a05c46d8f0ff84af03b3f31e5e
jnka070 <juansalamea94@gmail.com>
2020-10-30 12:47:09 -0500

    make it better

--- src/controllers/customercontrollers.js
+++ src/controllers/customercontrollers.js
@@ -1,25 +1,26 @@
-
 const primerobjeto = {};
 
-primerobjeto.list = function (req, res) {
-    req.getConnection(function (err, conn) {
-        conn.query('select * from usuario', function (err, registros) {
+primerobjeto.list = function(req, res) {
+    req.getConnection(function(err, conn) {
+        conn.query('select * from usuario', function(err, registros) {
             if (err) {
                 res.json(command);
             }
             console.log(registros);
             res.render('registro', {
-                dato: registros
+                data: registros
             });
         });
 
     });
 };
 
-primerobjeto.save = function (req, res) {
-    const dat = req.body;//aqui tengo todos los datos que viene del formulario
-    req.getConnection(function (err, conn) {
-        conn.query('INSERT INTO usuario SET ?', [dat], function (err, registers) {
+primerobjeto.save = function(req, res) {
+    const dat = req.body;
+
+    //aqui tengo todos los datos que viene del formulario
+    req.getConnection(function(err, conn) {
+        conn.query('INSERT INTO usuario SET ?', [dat], function(err, registers) {
 
             if (err) {
                 console.log(err);
@@ -28,13 +29,13 @@ primerobjeto.save = function (req, res) {
         });
     });
 }
-primerobjeto.edit = function (req, res) {
+primerobjeto.edit = function(req, res) {
     const dat = req.params.id;
-    req.getConnection(function (err, conn) {
+    req.getConnection(function(err, conn) {
         if (err) {
             console.log(err);
         }
-        conn.query('SELECT * FROM usuario WHERE id = ?', [dat], function (err, result) {
+        conn.query('SELECT * FROM usuario WHERE id = ?', [dat], function(err, result) {
             if (err) {
                 console.log(err);
             }
@@ -44,30 +45,53 @@ primerobjeto.edit = function (req, res) {
         });
     });
 }
-primerobjeto.update = function (req, res, next) {
+primerobjeto.update = function(req, res, next) {
     const { id } = req.params;
     const nuevodato = req.body;
-    req.getConnection(function (err, conn) {
-        conn.query('update usuario set ? where id = ?', [nuevodato, id], function (err, result) {
+    req.getConnection(function(err, conn) {
+        conn.query('update usuario set ? where id = ?', [nuevodato, id], function(err, result) {
             if (result) {
                 res.redirect('/');
 
             }
-            next();//llamar a next para que cause Error [ERR_HTTP_HEADERS_SENT]:
+            next(); //llamar a next para que cause Error [ERR_HTTP_HEADERS_SENT]:
         });
     });
 
 }
-primerobjeto.delete = function (req, res) {
+primerobjeto.delete = function(req, res) {
     const dat = req.params.id;
-    req.getConnection(function (err, conn) {
-        conn.query('delete from usuario where id=?', [dat], function (err, result) {
+    req.getConnection(function(err, conn) {
+        conn.query('delete from usuario where id=?', [dat], function(err, result) {
             if (err) {
                 console.log(err);
             }
             res.redirect('/');
         });
-        
+
+    })
+}
+primerobjeto.search = function(req, res, next) {
+    const id = req.body.id;
+    const nombres = req.body.id;
+    const apellidos = req.body.id;
+    const direccion = req.body.id;
+    const email = req.body.id;
+
+    req.getConnection((err, conn) => {
+        if (err) {
+            next(err);
+        } else {
+            conn.query('select * from usuario where nombres like ? OR id like ? OR apellidos like ? OR direccion like ? OR email like ?', ['%' + id + '%', '%' + nombres + '%', '%' + apellidos + '%', '%' + direccion + '%', '%' + email + '%'], (err, result) => {
+                if (err) {
+                    next(err);
+                } else {
+                    res.render('registro', {
+                        data: result
+                    })
+                }
+            });
+        }
     })
 }
 module.exports = primerobjeto;
--- src/routes/customer.js
+++ src/routes/customer.js
@@ -1,11 +1,13 @@
-const express= require('express');
-const router=express.Router();
-const cuscontroller=require('../controllers/customercontrollers');
+const express = require('express');
+const router = express.Router();
+const cuscontroller = require('../controllers/customercontrollers');
 
-router.get('/',cuscontroller.list);
+router.get('/', cuscontroller.list);
 router.post('/register', cuscontroller.save);
 router.get('/delete/:id', cuscontroller.delete);
 
 router.get('/update/:id', cuscontroller.edit);
 router.post('/update/:id', cuscontroller.update);
-module.exports=router;
+
+router.post('/search', cuscontroller.search);
+module.exports = router;
